ac21c30a21d8ff6a016b4c6d38d1aabd8e3cc97a
Sofiia Tsepotan <zepotan.sonja@gmail.com>
2025-11-29 09:47:11 -0500

    Fixed routing

--- termProject/src/app/tabs/tabs-routing.module.ts
+++ termProject/src/app/tabs/tabs-routing.module.ts
@@ -25,14 +25,14 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/students',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/students',
     pathMatch: 'full'
   }
 ];
