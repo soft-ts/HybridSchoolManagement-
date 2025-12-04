38ed6f7c1fa788172c082ae2fa0d0c20b0717aef
huythoren12 <huyle@sheridancollege.ca>
2025-12-04 18:43:06 -0500

    Fix Tab2Page, add CRUD functions and UI improvements

--- data/index.js
+++ data/index.js
@@ -23,6 +23,67 @@ app.listen(3000, ()=>
 // Courses CRUD
 
 // Students CRUD
+app.get("/showRecords", (req, res) => {
+  dbase.collection("students")
+    .find({})
+    .toArray((err, data) => {
+      if (err) res.json({ error: err });
+      else res.json(data);
+    });
+});
+
+
+app.post("/addRecord", (req, res) => {
+  dbase.collection("students").insertOne(
+    {
+      id: req.body.id,
+      lastName: req.body.lastName,
+      firstName: req.body.firstName,
+      course: req.body.course,
+      gpa: req.body.gpa
+    },
+    (err, result) => {
+      if (err) {
+        res.json({ error: err })}
+      else 
+        {res.json("Student added successfully!")};
+    }
+  );
+});
+
+
+app.post("/deleteRecord", (req, res) => {
+  dbase.collection("students").deleteOne(
+    { id: req.body.id },
+    (err, result) => {
+      if (err) 
+        {res.json({ error: err })}
+      else
+        { res.json("Student deleted successfully!")};
+    }
+  );
+});
+
+
+app.post("/updateRecord", (req, res) => {
+  dbase.collection("students").updateOne(
+    { id: req.body.id },
+    {
+      $set: {
+        lastName: req.body.lastName,
+        firstName: req.body.firstName,
+        course: req.body.course,
+        gpa: req.body.gpa
+      }
+    },
+    (err, result) => {
+      if (err)
+        { res.json({ error: err })}
+      else
+        { res.json("Student updated successfully!")};
+    }
+  );
+});
 
 // Instructors CRUD
 
